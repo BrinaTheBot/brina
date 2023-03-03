7d86fff413a90ad0361d4bda496c870db07fbf85
ashtrindade <92398466+ashtrindade@users.noreply.github.com>
2023-03-03 16:32:01 -0300

    feat: STT is working

--- src/commands/stt/join.js
+++ src/commands/stt/join.js
@@ -1,20 +1,21 @@
 const Client = require('../../../index').Client
 const { EmbedBuilder } = require('discord.js')
-const { joinVoiceChannel, EndBehaviorType } = require('@discordjs/voice')
-const { OpusEncoder } = require('@discordjs/opus')
+const { joinVoiceChannel } = require('@discordjs/voice')
+const DiscordVoice = require('@discordjs/voice')
 const vosk = require('vosk')
+const prism = require('prism-media')
 
 module.exports.run = async (inter) => {
     try {
         const channel = inter.channel.id
-        /* ---- If user is not in a voice channel ---- */
+        // If user is not in a voice channel
         const noChannel = new EmbedBuilder()
             .setColor('Orange')
             .setDescription('Entre em um canal de voz antes de usar o comando `/join`!')
 
         if (!inter.member.voice.channel) { return await inter.reply({ embeds: [noChannel] }) }
 
-        /* ---- If user is in a voice channel ---  */
+        // If user is in a voice channel
         // Create voice connection
         const connection = joinVoiceChannel({
             channelId: inter.member.voice.channel.id,
@@ -39,75 +40,84 @@ module.exports.run = async (inter) => {
         //connection.receiver.speaking.on('end', userId => console.log(`User <@${userId}> stopped speaking 🔈`))
 
         //----------------- TESTING NEW STUFF ----------------------------//
+        speak_impl(connection.receiver)
+
+        async function convert_audio(input) {
+            try {
+                // stereo to mono channel
+                const data = new Int16Array(input)
+                const ndata = data.filter((el, idx) => idx % 2);
+                return Buffer.from(ndata);
+            } catch (e) {
+                console.log(e)
+                console.log('convert_audio: ' + e)
+                throw e;
+            }
+        }
+
         vosk.setLogLevel(-1)
-        const ptModel = new vosk.Model('E:/Projetos/Brina-bot/voskModels/pt')
+        const ptModel = new vosk.Model('E:/Projetos/Brina-bot/src/voskModels/pt')
         const rec = new vosk.Recognizer({ model: ptModel, sampleRate: 48000 })
 
-        // prevent from listening to bots
-        connection.receiver.speaking.on('start', async (user) => {
-            if (user.bot) return
-            console.log(`Listening to <@${user}>`)
 
-            const opusStream = connection.receiver.subscribe(user, {
-                end: {
-                    behavior: EndBehaviorType.AfterSilence,
-                    duration: 200,
-                }
-            })
+        function speak_impl(receiver) {
+            connection.receiver.speaking.on('start', async (user) => {
+                if (user.bot) { return }
+                console.log(`Listening to <@${user}>`)
 
-            let buffer = []
-            opusStream.on('data', (data) => {
-                buffer.push(data)
-            })
+                const audioStream = receiver.subscribe(user, {
+                    end: {
+                        behavior: DiscordVoice.EndBehaviorType.AfterSilence,
+                        duration: 100,
+                    }
+                })
+                const decodedAudioStream = new prism.opus.Decoder({
+                    channels: 2,
+                    rate: 48000,
+                    frameSize: 960
+                })
 
-            opusStream.on('end', async () => {
-                buffer = Buffer.concat(buffer)
-                const duration = buffer.length / 48000 / 4
-                console.log('duration: ' + duration)
+                audioStream.pipe(decodedAudioStream)
+                decodedAudioStream.on('error', (e) => {
+                    console.log('audioStream: ' + e)
+                })
 
-                async function convert_audio(input) {
+                let buffer = []
+
+                decodedAudioStream.on('data', (data) => {
+                    buffer.push(data)
+                })
+
+                decodedAudioStream.on('end', async () => {
+                    buffer = Buffer.concat(buffer)
+                    const duration = buffer.length / 48000 / 4
+
+                    console.log("duration: " + duration)
+
                     try {
-                        // stereo to mono channel
-                        const data = new Int16Array(input)
-                        const ndata = data.filter((el, idx) => idx % 2);
-                        return Buffer.from(ndata);
-                    } catch (error) {
-                        console.log(error)
-                        console.log('convert_audio: ' + e)
-                        throw e;
+                        let new_buffer = await convert_audio(buffer)
+                        let out = await transcribe(new_buffer)
+                        if (out != null)
+                            process_commands_query(out, user)
+                    } catch (e) {
+                        console.log('tmpraw rename: ' + e)
                     }
-                }
-
-                try {
-                    let new_buffer = await convert_audio(buffer)
-                    let out = await transcribe(new_buffer, channel)
-                    if (out != null) {
-                        transcript(out, channel, user)
-                    }
-                } catch (error) {
-                    console.log('buffer: ' + e)
-                }
-
-                async function transcribe(buffer) {
-                    rec.acceptWaveform(buffer)
-                    let ret = rec.result().text // this isn't working
-                    // IT'S WORKING SO FAR
-                    console.log('vosk:' + ret)
-                    return ret
-                }
-
-                function transcript(txt, user) {
-                    if (txt && txt.length) {
-                        //Client.channels.cache.send(user.username + ': ' + txt)
-                        console.log(user.username + ': ' + txt)
-                    }
-                }
+                })
             })
-        })
 
+            async function process_commands_query(txt, user) {
+                if (txt && txt.length) {
+                    await inter.channel.send(`<@${user}>: ${txt}`)
+                }
+            }
 
-
-
+            async function transcribe(buffer) {
+                await rec.acceptWaveform(buffer)
+                let ret = await rec.result().text
+                console.log('vosk:', ret)
+                return ret
+            }
+        }
     } catch (error) {
         const erro = new EmbedBuilder()
             .setColor('Yellow')
