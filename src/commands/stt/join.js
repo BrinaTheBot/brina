2d050f02632b6204debb2a9574d52c4dcfc2fc16
ashtrindade <92398466+ashtrindade@users.noreply.github.com>
2023-03-18 16:20:05 -0300

    move models to assets

--- src/commands/stt/join.js
+++ src/commands/stt/join.js
@@ -1,22 +1,20 @@
-const Client = require('../../../index').Client
 const { EmbedBuilder } = require('discord.js')
 const { joinVoiceChannel } = require('@discordjs/voice')
 const DiscordVoice = require('@discordjs/voice')
 const vosk = require('vosk')
 const prism = require('prism-media')
+const path = require('path')
 
 module.exports.run = async (inter) => {
     try {
-        const channel = inter.channel.id
-        // If user is not in a voice channel
+        await inter.deferReply()
+
         const noChannel = new EmbedBuilder()
             .setColor('Orange')
             .setDescription('Entre em um canal de voz antes de usar o comando `/join`!')
 
-        if (!inter.member.voice.channel) { return await inter.reply({ embeds: [noChannel] }) }
+        if (!inter.member.voice.channel) { return await inter.editReply({ embeds: [noChannel] }) }
 
-        // If user is in a voice channel
-        // Create voice connection
         const connection = joinVoiceChannel({
             channelId: inter.member.voice.channel.id,
             guildId: inter.channel.guild.id,
@@ -25,45 +23,37 @@ module.exports.run = async (inter) => {
             selfMute: true,
         })
 
-        // join channel
         connection
 
-        // Interaction reply        
         const conectado = new EmbedBuilder()
             .setColor('Green')
             .setDescription('Estou conectada')
 
-        await inter.reply({ embeds: [conectado] })
+        await inter.editReply({ embeds: [conectado] })
 
-        // voice receiver log
-        //connection.receiver.speaking.on('start', userId => console.log(`User <@${userId}> started speaking 🔊`))
-        //connection.receiver.speaking.on('end', userId => console.log(`User <@${userId}> stopped speaking 🔈`))
+        voiceEntry(connection.receiver)
 
-        //----------------- TESTING NEW STUFF ----------------------------//
-        speak_impl(connection.receiver)
-
-        async function convert_audio(input) {
+        async function convertAudioStereoToMono(input) {
             try {
-                // stereo to mono channel
                 const data = new Int16Array(input)
                 const ndata = data.filter((el, idx) => idx % 2);
                 return Buffer.from(ndata);
             } catch (e) {
                 console.log(e)
-                console.log('convert_audio: ' + e)
+                console.log('convertAudioStereoToMono: ' + e)
                 throw e;
             }
         }
 
         vosk.setLogLevel(-1)
-        const ptModel = new vosk.Model('E:/Projetos/Brina-bot/src/voskModels/pt')
-        const rec = new vosk.Recognizer({ model: ptModel, sampleRate: 48000 })
+        const pathToPt = path.resolve('src/assets/transcriber/voskModels/', 'pt/')
+        const pt = new vosk.Model(pathToPt)
+        const rec = new vosk.Recognizer({ model: pt, sampleRate: 48000 })
 
 
-        function speak_impl(receiver) {
+        function voiceEntry(receiver) {
             connection.receiver.speaking.on('start', async (user) => {
                 if (user.bot) { return }
-                console.log(`Listening to <@${user}>`)
 
                 const audioStream = receiver.subscribe(user, {
                     end: {
@@ -92,36 +82,42 @@ module.exports.run = async (inter) => {
                     buffer = Buffer.concat(buffer)
                     const duration = buffer.length / 48000 / 4
 
-                    console.log("duration: " + duration)
-
                     try {
-                        let new_buffer = await convert_audio(buffer)
+                        let new_buffer = await convertAudioStereoToMono(buffer)
                         let out = await transcribe(new_buffer)
                         if (out != null)
-                            process_commands_query(out, user)
+                            processCommandsQuery(out, user)
                     } catch (e) {
                         console.log('tmpraw rename: ' + e)
                     }
                 })
             })
 
-            async function process_commands_query(txt, user) {
+            async function processCommandsQuery(txt, user) {
                 if (txt && txt.length) {
-                    await inter.channel.send(`<@${user}>: ${txt}`)
+                    try {
+                        const transcription = new EmbedBuilder()
+                            .setColor('F1A7AE')
+                            .setDescription(`<@${user}>: ${txt}`)
+
+                        await inter.channel.send({ embeds: [transcription] })
+                    } catch (error) {
+                        console.log(error)
+                    }
                 }
             }
 
             async function transcribe(buffer) {
                 await rec.acceptWaveform(buffer)
                 let ret = await rec.result().text
-                console.log('vosk:', ret)
                 return ret
             }
         }
     } catch (error) {
         const erro = new EmbedBuilder()
             .setColor('Yellow')
-            .setDescription('Oh não, ocorreu um erro!\n Caso isso persista, contate os desenvolvedores.')
+            .setTitle('Oh não, ocorreu um erro!')
+            .setDescription('Caso isso persista, contate os desenvolvedores.')
 
         await inter.editReply({ embeds: [erro] })
 
