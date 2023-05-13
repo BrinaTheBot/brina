fce9efd8a5dc07947585047ff2710284f7c1324c
ashtrindade <92398466+ashtrindade@users.noreply.github.com>
2023-05-13 19:07:23 -0300

    db: added events

---
+++ src/events/guildDelete.js
@@ -0,0 +1,10 @@
+const Client = require('../../index').Client
+const deleteGuildOperation = require('../utils/guild/deleteGuildOperation')
+
+Client.on('guildDelete', async (guild) => {
+  try {
+    deleteGuildOperation(guild)
+  } catch (error) {
+    console.log(error)
+  }
+})
