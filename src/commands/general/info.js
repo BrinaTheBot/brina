ab919227630fd14114acf9c6a31b5e0db3252968
ashtrindade <92398466+ashtrindade@users.noreply.github.com>
2023-05-20 15:06:11 -0300

    fix: update info cmd

--- src/commands/general/info.js
+++ src/commands/general/info.js
@@ -12,30 +12,7 @@ module.exports.run = async (inter) => {
       })
       .setTitle('Olá, eu sou a Brina')
       .setDescription(
-        'Brina é um bot de acessibilidade que oferece funções de transcrição de texto. Ela foi criada para ajudar a tornar o Discord mais acessível para usuários com deficiência, permitindo que eles aproveitem totalmente a plataforma.'
-      )
-      .addFields(
-        { name: '\u200B', value: '\u200B' },
-        {
-          name: 'Features:',
-          value: '• Transcrição em pt-BR e en-US. \n*(em breve mais idiomas)*'
-        },
-        {
-          name: 'Em breve:',
-          value:
-            '• Localização dos comandos. \n• OCR (Optical character recognition)'
-        },
-        {
-          name: 'Contribua:',
-          value:
-            '• Brina é um projeto de código aberto. \nContribua: [Github](https://github.com/ashtrindade/brina)'
-        },
-        { name: '\u200B', value: '\u200B' },
-        {
-          name: 'Icon:',
-          value:
-            '• Esse icon lindo foi feito por [Nikkoe](https://twitter.com/NikkoeNi).'
-        }
+        'Brina é um bot de acessibilidade que oferece funções de transcrição de texto. Ela foi criada para ajudar a tornar o Discord mais acessível para usuários com deficiência, permitindo que eles aproveitem totalmente a plataforma. \n\n:woman_student_tone3: **Features:**\n- Transcrição em pt-BR e en-US. *(em breve mais idiomas)* \n\n:woman_mechanic_tone3: **Em breve:**\n- Localização dos comandos.\n- OCR (Optical character recognition) \n\n:woman_raising_hand_tone3: **Contribua:** \nBrina é um projeto de código aberto: [Github](https://github.com/ashtrindade/brina).\n\n:sparkles: Icon :sparkles: \nEsse icon lindo foi feito por [Nikkoe](https://twitter.com/NikkoeNi).'
       )
       .setImage(
         'https://user-images.githubusercontent.com/92398466/227223245-3e890f87-26cb-45d8-a091-9623b6390b71.jpg'
